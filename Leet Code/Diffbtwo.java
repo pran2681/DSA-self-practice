39904660b01f45f7e9e2990ee9fdcffb5689b8e0
Pran Prachurjya Saikia <92747138+pran2681@users.noreply.github.com>
2023-02-26 23:54:55 +0530

    Add files via upload

---
+++ Leet Code/Diffbtwo.java
@@ -0,0 +1,25 @@
+// import java.util.Scanner;
+
+// public class Diffbtwo{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+    
+//     int[] arr = new int[size];
+//     System.out.println("enter elements: ");
+//     for(int i=0;i<size;i++){
+//       arr[i]=sc.nextInt();
+//     }
+//     int eleSum = 0;
+//     int digiSum = 0;
+//     for(int i=0;i<size;i++){
+//       eleSum+=arr[i];
+//       int n=arr[i];
+//       while(n>0){
+//         digiSum+=n%10;
+//         n/=10;
+//       }
+//     }
+//     System.out.println(eleSum-digiSum);
+//     sc.close();
+//   }
+// }
