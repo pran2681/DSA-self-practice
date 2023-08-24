b824dcaeae12aa1a1ba23160c52c09dbda259550
Pran Prachurjya Saikia <92747138+pran2681@users.noreply.github.com>
2023-08-24 23:52:32 +0530

    Add files via upload

---
+++ Leet Code/SearchInsertPosition.java
@@ -0,0 +1,15 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int q = 0, w = nums.length-1,r=-1;
+        while (q <= w) {
+            r = (w + q) >> 1;
+            if (nums[r] > target)
+                w = r - 1;
+            else if (target > nums[r])
+                q = r + 1;
+            else
+                return r;
+        }
+        return nums[r]>target?r:r+1;
+    }
+}
