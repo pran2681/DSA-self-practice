85766d7dd8ed556c28368dc1c069dc1545202cc4
Pran Prachurjya Saikia <92747138+pran2681@users.noreply.github.com>
2023-08-27 02:03:02 +0530

    Add files via upload

---
+++ Coding blocks QSN/Array/ProductOfArray1.java
@@ -0,0 +1,18 @@
+public class ProductOfArray1 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+
+    }
+    public static void solve(int[] arr)[
+        for(int i=0;i<arr,length;i++){
+            int p_L=1;
+            for(int j=0;j<=i-1;j++){
+                p_L=p_L*arr[j];
+            }
+            int p_R=1;
+            for(int j=i+1;j<arr.length;j++){
+                p_R=p_L
+            }
+        }
+    ]
+}
